ee9cc703dba8bee69ff5f80825df528decd31989
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2025-03-31 16:20:44 +0200

    product added to product page

--- ecommerce-client/src/pages/Products.tsx
+++ ecommerce-client/src/pages/Products.tsx
@@ -1,3 +1,39 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { IProduct } from "../../../ecommerce-api/src/models/IProduct";
+
 export const Products = () => {
-  return <div>Products</div>;
+  const [productList, setProductList] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    axios.get<IProduct[]>("http://localhost:3000/products").then((response) => {
+      setProductList(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <h1 className="my-20 text-5xl ">Wall Clock</h1>
+      <div className="flex gap-10">
+        {productList.map((product) => {
+          return (
+            <div
+              className="size-120 overflow-hidden hover:font-semibold cursor-pointer"
+              key={product.id}
+            >
+              <img
+                className="object-cover h-8/9 w-full"
+                src={product.image}
+                alt={product.name}
+              />
+              <div className="flex justify-between">
+                <h3 className="w-1/2 truncate"> {product.name} </h3>
+                <p>{product.price} sek</p>
+              </div>
+              <p className="w-full truncate ">{product.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
